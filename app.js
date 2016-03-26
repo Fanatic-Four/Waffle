8051dfb1a67e97729b0c84c0ab595ff6fc0525f1
DeveloperTommy <its.tommy.nguyen@gmail.com>
2016-03-25 23:40:48 -0400

    Debugging

--- app.js
+++ app.js
@@ -1,119 +1,18 @@
-// This requires the necessary libraries for the webapp.
-// (1) express - this provides the express web framework
-// (2) handlebars - this provides the handlebars templating framework
-var express    = require('express');
-var handlebars = require('express-handlebars');
-
-//////////////////////////////////////////////////////////////////////
-///// Express App Setup //////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
+var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-
-var view = handlebars.create({ defaultLayout: 'main' });
-app.engine('handlebars', view.engine);
-app.set('view engine', 'handlebars');
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
-function testmw(req, res, next) {
-  // This checks the 'env' application variable to determine if we are
-  // in "production" mode. An application is in "production" mode if
-  // it is actually deployed. This can be set by the NODE_ENV
-  // environment variable. It also checks to see if the request has
-  // given a `test` querystring parameter, such as
-  // http://localhost:3000/about?test=1. If the route has that set
-  // then showTests will be set to a "truthy" value. We can then
-  // use that in our handlebars views to conditionally include tests.
-  res.locals.showTests = app.get('env') !== 'production' &&
-                         req.query.test;
-  // Passes the request to the next route handler.
-  next();
-}
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-// This adds our testing middleware to the express app.
-app.use(testmw);
-
-//////////////////////////////////////////////////////////////////////
-///// User Defined Routes ////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-var team = require('./lib/team.js');
-
-var users = require('./lib/users.js');
-var events = require('./lib/events.js');
-
-
-app.get('/', (req, res) => {
-  // TODO
-  var result = team.all();
-  if (!result.success) {
-    notFound404(req, res);
-  } else {
-    res.render('team', {
-      members: result.data,
-      pageTestScript: '/qa/tests-team.js'
-    });
-  }
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-//Also added a route for /team since I think that seems to make sense
-app.get('/team', (req, res) => {
-  // TODO
-  var result = team.all();
-  if (!result.success) {
-    notFound404(req, res);
-  } else {
-    res.render('team', {
-      members: result.data,
-      pageTestScript: '/qa/tests-team.js'
-    });
-  }
-});
-
-
-//////////////////////////////////////////////////////////////////////
-///// Error Middleware ///////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-// A middleware function that will be invoked if no other route path
-// has been matched. HTTP 404 indicates that the resource was not
-// found. We set the HTTP status code in the response object to 404.
-// We then render our views/404.handlebars view back to the client.
-function notFound404(req, res) {
-  res.status(404);
-  res.render('404');
-}
-
-// A middleware function that will be invoked if there is an internal
-// server error (HTTP 500). An internal server error indicates that
-// a serious problem occurred in the server. When there is a serious
-// problem in the server an additional `err` parameter is given. In
-// our implementation here we print the stack trace of the error, set
-// the response status code to 500, and render our
-// views/500.handlebars view back to the client.
-function internalServerError500(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
-}
-
-// This adds the two middleware functions as the last two middleware
-// functions. Because they are at the end they will only be invoked if
-// no other route defined above does not match.
-app.use(notFound404);
-app.use(internalServerError500);
-
-//////////////////////////////////////////////////////////////////////
-//// Application Startup /////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-// Starts the express application up on the port specified by the
-// application variable 'port' (which was set above). The second
-// parameter is a function that gets invoked after the application is
-// up and running.
-app.listen(app.get('port'), () => {
-  console.log('Express started on http://localhost:' +
-              app.get('port') + '; press Ctrl-C to terminate');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port:', app.get('port'));
 });
